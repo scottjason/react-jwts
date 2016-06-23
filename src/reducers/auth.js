14502dc054017d236de2e256fb4a2f790610c007
Scott Jason <scottleejason@gmail.com>
2016-06-23 01:32:04 -0700

    add back breaks in switch statement

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -8,10 +8,14 @@ export default function auth(state = initialState, action) {
   switch (action.type) {
     case TOGGLE_AUTH:
       state.isLoginView = !state.isLoginView
+      return state
+      break
     case LOGIN:
     case SIGNUP:
       state.user = typeof action.data.user === 'object' ? action.data.user : undefined
       state.message = typeof action.data.message === 'string' ? action.data.message : undefined
+      return state
+      break
     default:
       return state
   }
